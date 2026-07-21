3ad2d78c221bc99690f364fdf3394d42d112f7a1
Alaashamel <Alaashamel32@gmail.com>
2026-07-21 16:57:51 +0300

    Add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="h-16 border-b flex items-center px-6 bg-white">
+      <h1 className="text-xl font-bold">
+        TaskFlow
+      </h1>
+    </nav>
+  );
+}
