5659554625c0170cb1e606b64aa39f40f3d892e5
Alaashamel <Alaashamel32@gmail.com>
2026-07-21 16:59:45 +0300

    Add Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+  return (
+    <aside className="w-64 min-h-screen bg-gray-900 text-white p-5">
+      <ul className="space-y-4">
+        <li>Dashboard</li>
+        <li>Projects</li>
+        <li>Tasks</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+}
